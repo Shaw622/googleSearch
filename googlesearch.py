04283b4738d057223b6389b760cba1260092111a
Shaw622 <46571592+Shaw622@users.noreply.github.com>
2019-05-23 00:34:16 +0900

    Update googlesearch.py
    Removed "TODO" from "#各結果をブラウザのタブで開く"

--- googlesearch.py
+++ googlesearch.py
@@ -11,7 +11,7 @@ res.raise_for_status()
 soup = bs4.BeautifulSoup(res.text)
 link_elems = soup.select('.r a')
 
-#TODO 各結果をブラウザのタブで開く
+#各結果をブラウザのタブで開く
 num_open = min(5, len(link_elems))
 for i in range(num_open):
     webbrowser.open('http://google.com' + link_elems[i].get('href'))
